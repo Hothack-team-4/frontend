c6b74ccb80c70684290d96f4dd0405c9056c59e1
Veneciac <veneciacalista00@gmail.com>
2024-01-20 19:40:16 +1000

    remove error

--- pages/artist.tsx
+++ pages/artist.tsx
@@ -1,37 +1,33 @@
 import React from "react";
-import { useRef } from 'react';
-import codeGen from "../utils/qrCodeGenerator.js";
+import { useRef } from "react";
 import "./artiststyle.css";
 // import DashboardLandingPage from "../pages/dashboard.tsx"
 
 // this page will be for the attendees on the venue when they scan the QR
 
 const DashboardLandingPage = () => {
-    const myRef = useRef(null);
-    const code = codeGen("Hello this is a filler");
-    console.log(code);
-    return <div id= "document-body">
-    <div>
+  return (
+    <div id="document-body">
+      <div>
         <h1>HELLO</h1>
-    </div>
-    <div id="current-event">
+      </div>
+      <div id="current-event">
         <h2>CURRENT EVENT</h2>
         {/* JS insert: name/date of event or no event found*/}
         <div id="stats">
-            <h2>NUMBER SCANNED</h2>
+          <h2>NUMBER SCANNED</h2>
         </div>
-        <div id= "QR-code">
-            <div id= "placeholder-for-qrcode"></div>
-            <button>DOWNLOAD</button>
+        <div id="QR-code">
+          <div id="placeholder-for-qrcode"></div>
+          <button>DOWNLOAD</button>
         </div>
-    </div>
-    <div>
+      </div>
+      <div>
         <button>Create New Event!</button>
+      </div>
     </div>
-        
-    </div>
+  );
 };
 
-
 //export { DashboardLandingPage, qrcodeGenerator }
-export default DashboardLandingPage
+export default DashboardLandingPage;

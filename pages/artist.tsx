eb163e9dc14df80283a2129aa12f56fd2a0745b3
ibehanwillett <imogenbdubs@gmail.com>
2024-01-20 13:22:45 +1000

    Basic HTML for artist dashboard created.

---
+++ pages/artist.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+
+// this page will be for the artist dashboard
+const DashboardLandingPage = () => {
+
+    return <html lang="en">
+    <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Artist Dashboard</title>
+    </head>
+    <body>
+    <div>
+        <div id="image-placeholder"></div>
+        <h1>HELLO</h1>
+    </div>
+    <section id="current-event">
+        <h2>CURRENT EVENT</h2>
+        <section id="stats">
+            <h2>NUMBER SCANNED</h2>
+            <p>-</p>
+        </section>
+        <div id= "QR-code">
+            <div id= "placeholder-for-qrcode"></div>
+            <button>DOWNLOAD</button>
+        </div>
+    </section>
+    <button>Create New Event!</button>
+
+        
+    </body>
+    </html>
+};
+
+
+export default DashboardLandingPage

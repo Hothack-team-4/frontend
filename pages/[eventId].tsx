e0bb1885d742bdeae00edab17e8fe179061b73f4
Veneciac <veneciacalista00@gmail.com>
2024-01-20 10:20:04 +1000

    project structure

---
+++ pages/[eventId].tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+// this page will be for the attendees on the venue when they scan the QR
+const AttendanceLandingPage = () => {
+
+    return <div>
+        Hello this is attendance landing page
+    </div>
+};
+
+
+export default AttendanceLandingPage

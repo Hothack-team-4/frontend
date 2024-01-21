dd2bc96c9a45f5a7be2213d85c2a45b91ed5f0f1
Valentinas <vkornijenka@gmail.com>
2024-01-21 12:31:46 +1000

    styled qr cards

--- components/EventList/index.tsx
+++ components/EventList/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { collection, getDocs } from "firebase/firestore";
-
+import Button from 'react-bootstrap/Button';
 import Card from "react-bootstrap/Card";
 import "./card-style.css";
 
@@ -8,24 +8,30 @@ const EventList = ({ list }: any) => {
   return (
     <div>
       <div className="qr-code-cards-container">
-      {list.map((event: any) => {
-        return (
-          <Card key={event.name}
-          style={{}} className="qr-code-card">
-            <Card.Body className="qr-code-body">
-              <div className="qr-code-wrapper">
-              <div id={event.name} className="qr-code" />
-              </div>
-              <div className="event-details">
-                  {event.name}
-                  <br/>
-                  Attendees: {event.attendees.length}
+        {list.map((event: any) => {
+          return (
+            <Card key={event.name} className="qr-code-card">
+              <Card.Body className="qr-code-body">
+                <div className="qr-code-wrapper">
+                  <div id={event.name} className="qr-code" />
                 </div>
-            </Card.Body>
-          </Card>
-        );
-      })}
-    </div>
+                <div className="event-details">
+                  <div className="event-name">{event.name}</div>
+                  <div className="event-attendees">
+                    Attendees: {event.attendees.length}
+                  </div>
+                  <Button 
+                    variant="primary" 
+                    href={`/${event.name}`} 
+                    className="event-link-button">
+                    View Artist
+                  </Button>
+                </div>
+              </Card.Body>
+            </Card>
+          );
+        })}
+      </div>
     </div>
   );
 };

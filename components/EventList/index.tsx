e6dbe20b45d686bbb8c0294872d1919dc4221cab
Valentinas <vkornijenka@gmail.com>
2024-01-21 10:36:53 +1000

    index.tsx added wrapper

--- components/EventList/index.tsx
+++ components/EventList/index.tsx
@@ -7,16 +7,20 @@ import "./card-style.css";
 const EventList = ({ list }: any) => {
   return (
     <div>
-      <h1>YOUR EVENT LIST</h1>
       <div className="qr-code-cards-container">
       {list.map((event: any) => {
         return (
           <Card key={event.name}
           style={{}} className="qr-code-card">
             <Card.Body className="qr-code-body">
-              {event.name}
-              Attendees: {event.attendees.length}
+              <div className="qr-code-wrapper">
               <div id={event.name} className="qr-code" />
+              </div>
+              <div className="event-details">
+                  {event.name}
+                  <br/>
+                  Attendees: {event.attendees.length}
+                </div>
             </Card.Body>
           </Card>
         );

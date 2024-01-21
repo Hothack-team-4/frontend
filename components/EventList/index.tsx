88eb7aba2c55cb1aeb8a77adc9769afec081347b
Valentinas <vkornijenka@gmail.com>
2024-01-21 10:26:22 +1000

    index.tsx styling

--- components/EventList/index.tsx
+++ components/EventList/index.tsx
@@ -2,23 +2,26 @@ import React, { useState, useEffect } from "react";
 import { collection, getDocs } from "firebase/firestore";
 
 import Card from "react-bootstrap/Card";
+import "./card-style.css";
 
 const EventList = ({ list }: any) => {
   return (
     <div>
       <h1>YOUR EVENT LIST</h1>
+      <div className="qr-code-cards-container">
       {list.map((event: any) => {
         return (
           <Card key={event.name}
-          style={{}}>
-            <Card.Body>
+          style={{}} className="qr-code-card">
+            <Card.Body className="qr-code-body">
               {event.name}
               Attendees: {event.attendees.length}
-              <div id={event.name} />
+              <div id={event.name} className="qr-code" />
             </Card.Body>
           </Card>
         );
       })}
+    </div>
     </div>
   );
 };
